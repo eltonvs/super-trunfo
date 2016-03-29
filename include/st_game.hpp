0d082be687747829e635c62e9b6bc75caf746d58
Elton Viana <eltonvs@outlook.com>
2016-03-29 10:46:56 -0300

    Added initial structure

--- include/st_game.hpp
+++ include/st_game.hpp
@@ -1,9 +1,25 @@
 #ifndef _ST_Game_
 #define _ST_Game_
 
+#include <string>
+#include <vector>
+#include "st_player.hpp"
+
 class ST_Game {
  public:
+    // Methods
+    int run();
+    std::string winner();
+    bool readDeckFromFile(std::ifstream, std::string);
+    void addPlayer(ST_Player);
+    void dealCards(int);
+    void displayDeck();
+    void displayPlayers();
+    void displayStatusRound();
+    void displayCardsOnTable();
+
  private:
+    std::vector <ST_Player> m_players;
 };
 
 #endif
---
+++ src/st_game.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "st_game.hpp"
+
+// Methods
+bool readDeckFromFile(std::ifstream &_file, std::string file_name) {
+    //_file.open("data/" + file_name);
+    return true;
+}
