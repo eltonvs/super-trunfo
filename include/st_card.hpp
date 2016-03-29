e06be24e23a9e7cd4cebf2538e7cf46bf40367fd
pedroarthur-mf <pedroarthur-mf@hotmail.com>
2016-03-29 10:19:19 -0300

    stdcard

--- include/st_card.hpp
+++ include/st_card.hpp
@@ -3,7 +3,16 @@
 
 class ST_Card {
  public:
+ 	void Construtor();
+
+ 	std::string getAttribute();
+
+ 	void displayCard();
+
  private:
+
+ 	std::string card {std::string id, std::string name, std::string origem, int year, float comprimento, float envergadura};
+
 };
 
 #endif
