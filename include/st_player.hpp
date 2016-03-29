6bd0b713cbf7b2f451a7f6472343603225806844
Elton Viana <eltonvs@outlook.com>
2016-03-29 09:41:39 -0300

    Added ST_Player signatures

--- include/st_player.hpp
+++ include/st_player.hpp
@@ -1,9 +1,25 @@
 #ifndef _ST_Player_
 #define _ST_Player_
 
+#include <string>
+
 class ST_Player {
  public:
+    // Constructor
+    ST_Player(std::string name);
+    // Getters
+    std::string get_name();
+    int get_nCards();
+    // Setters
+    void addCard();
+    // Methods
+    ST_Card pop_card();
+    int chooseRandomAttrib();
+    void displayCards();
+
  private:
+    std::string m_name;
+    int m_nCards;
 };
 
 #endif
