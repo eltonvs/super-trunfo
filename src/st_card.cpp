2c52fcf6c8ad43dfe9836666b1ef865888253578
pedroarthur-mf <pedroarthur-mf@hotmail.com>
2016-03-29 10:11:26 -0300

    stcard

---
+++ src/st_card.cpp
@@ -0,0 +1,16 @@
+void Construtor(){
+
+}
+
+ std::string getAttribute(){
+
+ }
+
+ void displayCard(){
+ 	cout<< "ID: " << id 
+ 		<< "\nName: "<< name
+ 		<<"\nfabricante e pais de origem: "<< origem
+ 		"\nAno: "<< year
+ 		"\ncomprimento: "<< comprimento
+ 		"\nEnvergadura: "<< envergadura<< endl;
+ }
