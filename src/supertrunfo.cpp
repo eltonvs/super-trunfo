14fe5e1ce1f2b154ff250475ba480b4c0678258b
Elton Viana <eltonvs@outlook.com>
2016-03-30 02:10:10 +0000

    Fix main

--- src/supertrunfo.cpp
+++ src/supertrunfo.cpp
@@ -33,10 +33,9 @@ int main(int argc, char const *argv[]) {
     // (3): Show deck on screen.
     myGame.displayDeck();
 
-    // (4): Create and add 3 players to the game object.
-    myGame.addPlayer(new ST_Player("Player #1"));
-    myGame.addPlayer(new ST_Player("Player #2"));
-    myGame.addPlayer(new ST_Player("Player #3"));
+    // (4): Create and add N_PLAYERS players to the game object.
+    for (int i = 0; i < N_PLAYERS; i++)
+        myGame.addPlayer(new ST_Player("Player #" + std::to_string(i + 1)));
 
     // (5): Deal the same amount of cards to all players.
     if (!myGame.dealCards(N_CARDS)) { // Deals N_CARDS to each player.
@@ -54,34 +53,3 @@ int main(int argc, char const *argv[]) {
 
     return EXIT_SUCCESS;
 }
-
-/*
-int main (int argc, char *argv[]) {
-    std::string cardsFileName; // Holds the cards file name.
-    std::ifstream inputFile;
-    // Stream object to handle the input file.
-    // (1): Process any command line arguments.
-    // (2): Create a game object.
-    ST_Game myGame;
-    if (!myGame.readDeckFromFile(inputFile)) {
-        // ERROR MESSAGE GOES HERE.
-    }
-    // (3): Show deck on screen.
-    myGame.displayDeck();
-    // (4): Create and add 3 players to the game object.
-    myGame.addPlayer(new ST_Player("Player #1"));
-    myGame.addPlayer(new ST_Player("Player #2"));
-    myGame.addPlayer(new ST_Player("Player #3"));
-    // (5): Deal the same amount of cards to all players.
-    if (!myGame.dealCards(N_CARDS)) { // Deals N_CARDS to each player.
-        // ERROR MESSAGE GOES HERE
-    }
-    // (6): Run a game with several rounds.
-    auto nRounds = myGame.run();
-    // (7): Get winner and display the winner.
-    auto winner = myGame.winner();
-    std::cout << "\n\n>>> The winner after " << nRounds
-              << " rounds played is : \" " << winner->name() << " \"\n\n";
-    return EXIT_SUCCESS;
-}
-*/
